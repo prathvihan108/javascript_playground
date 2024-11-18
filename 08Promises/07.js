3cc916017290ad32f377cf621f5f25a91d07aac6
Prathvihan1008 <prathvihan1008@gmail.com>
2024-11-18 19:34:08 +0530

    update

--- 08Promises/07.js
+++ 08Promises/07.js
@@ -1,3 +1,4 @@
+//https://youtu.be/U74BJcr8NeQ?si=Dlf1Qy3TNUeqclsc
 // E commarce example for the promises
 //createOrder()//Async operations
 //proceedToPayment()//Async operations
@@ -6,8 +7,9 @@
 const cart = ['item1','item2'];
 
 // const promise = createOrder(cart);
+//attach the function to then blocks
 createOrder(cart)
-.then((orderId)=>{
+.then((orderId)=>{  
     console.log(orderId);
     return proceedToPayment(orderId);
 
@@ -19,7 +21,7 @@ createOrder(cart)
 .then((state)=>{
     console.log(state);
 })
-.catch((error)=>{
+.catch((error)=>{ //attach a callback failure method to catch the errors.
     console.log(error)
 });
 //note:each then block returns(value or a another promise to move to next) to move to next
@@ -27,12 +29,15 @@ createOrder(cart)
 //the last catch block is capable of catching the any error(rejected state) on the top of it.
 //if u write the catch inbetween the then blocks then too its fine(it will only catch the errors above it) and the next blocks will continue executing .
 
-
+//Api side
+//Producers:
+//we can not call resolve/reject twice(first sattelement is final)
 function createOrder(cart){
     const pr = new Promise(function (resolve,reject){
+       
         if(!validateCart(cart)){
-            throw new Error("cart is not valid");
-            reject("Error in creating cart");
+            const error= new Error("cart is not valid");
+            reject(error);
         }
 
         else{
